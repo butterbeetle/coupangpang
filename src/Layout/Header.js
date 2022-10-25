729d8ac07f3aa40474162a49e80a2e7ff7c2cfcd
butterbeetle <butterbeetle@naver.com>
2022-10-25 23:50:24 +0900

    [3] header 견적 짜기

---
+++ src/Layout/Header.js
@@ -0,0 +1,37 @@
+const Header = () => {
+  return (
+    <header>
+      <section>
+        <div>
+          <p>카테고리</p>
+        </div>
+        <div>
+          <div>
+            <span>COUPANGPANG</span>
+            <p>검색창</p>
+            <ul>
+              <li>마이쿠팡</li>
+              <li>장바구니</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>로켓배송</li>
+              <li>로켓프레시</li>
+              <li>biz쿠팡비즈</li>
+              <li>로켓직구</li>
+              <li>골드박스</li>
+              <li>와우회원할인</li>
+              <li>이벤트/쿠폰</li>
+              <li>착한상점</li>
+              <li>기획전</li>
+              <li>여행/티켓</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
+import Header from "./Header";
 import Navigation from "./Navigation";
 
 const Layout = (props) => {
   return (
     <Fragment>
       <Navigation />
+      <Header />
       <main>{props.children}</main>
     </Fragment>
   );
