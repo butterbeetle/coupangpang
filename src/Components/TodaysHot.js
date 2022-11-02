71f7b4d8548877f3884d90b0dc64fee683a2a8a0
butterbeetle <butterbeetle@naver.com>
2022-11-02 21:28:25 +0900

    [11] Nav bar 높이 수정

--- src/Components/TodaysHot.js
+++ src/Components/TodaysHot.js
@@ -1,4 +1,11 @@
 import classes from "./TodaysHot.module.css";
+//banner img
+import today_item1 from "../img/banner/today_hot_banner_item1.png";
+import today_item2 from "../img/banner/today_hot_banner_item2.png";
+import today_item3 from "../img/banner/today_hot_banner_item3.png";
+import today_item4 from "../img/banner/today_hot_banner_item4.png";
+import today_item5 from "../img/banner/today_hot_banner_item5.png";
+import today_item6 from "../img/banner/today_hot_banner_item6.png";
 
 const TodaysHot = () => {
   return (
@@ -6,12 +13,36 @@ const TodaysHot = () => {
       <img />
       <div>
         <ul>
-          <li>a</li>
-          <li>b</li>
-          <li>c</li>
-          <li>d</li>
-          <li>e</li>
-          <li>f</li>
+          <li>
+            <a href="/">
+              <img src={today_item1} alt="배너1" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={today_item2} alt="배너1" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={today_item3} alt="배너1" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={today_item4} alt="배너1" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={today_item5} alt="배너1" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <img src={today_item6} alt="배너1" />
+            </a>
+          </li>
         </ul>
       </div>
     </section>
