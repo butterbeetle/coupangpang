9bb9766ee4492bff2531591b8c155ec09b290b94
butterbeetle <butterbeetle@naver.com>
2022-11-26 00:54:42 +0900

    [68] 로그인 상황 따라 색 넣기 준비

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,44 +1,78 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
-  const [focus, setFocus] = useState(false);
-  const [blur, setBlur] = useState(false);
+  // 현재 눌렀는지
+  const [isFocus, setIsFocus] = useState(false);
+  // 누르고 밖으로 나온거
+  const [isBlur, setIsBlur] = useState(false);
+  // 입력했는지?
+  const [isTouched, setIsTouched] = useState(false);
+
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    setColor(
+      (prevColor) =>
+        (prevColor = isFocus
+          ? isTouched
+            ? styles["invalid"]
+            : styles["focus"]
+          : "")
+    );
+  }, [isFocus, isTouched]);
 
   const focusHandler = () => {
-    setFocus((prev) => (prev = true));
-    setBlur((prev) => (prev = false));
+    setIsFocus((prev) => (prev = true));
+    setIsBlur((prev) => (prev = false));
   };
   const blurHandler = () => {
-    setFocus((prev) => (prev = false));
-    setBlur((prev) => (prev = true));
+    setIsFocus((prev) => (prev = false));
+    setIsBlur((prev) => (prev = true));
   };
-
-  console.log("focus", focus, "blur", blur);
+  const changeHandler = (event) => {
+    setIsTouched(true);
+    console.log("Input", event.target.value);
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
     console.log("Submit");
   };
+  console.log(
+    "isFocus=>",
+    isFocus,
+    "isBlur=>",
+    isBlur,
+    "isTouched=>",
+    isTouched,
+    "color=>",
+    color
+  );
 
   return (
     <Fragment>
-      <main className={styles.login__main}>
-        <form onSubmit={submitHandler}>
-          <div className={styles.login__main__id}>
-            <label
-              htmlFor="id"
-              className={styles.login__main__id__label}
-              onFocus={focusHandler}
-              onBlur={blurHandler}
-            >
-              <div>
-                <span className={styles.login__main__id__icon}></span>
-              </div>
-              <input id="id" placeholder="아이디(이메일)" type="text"></input>
-            </label>
-            {/* <p>아이디(이메일)를 입력해주세요.</p> */}
+      <main className={styles["login"]}>
+        <form className={styles["login__form"]} onSubmit={submitHandler}>
+          <div className={styles["login__form--id"]}>
+            <div className={styles["login__form--id--main"]}>
+              <label
+                htmlFor="id"
+                className={`${styles["login__form--label"]} ${color}`}
+                onFocus={focusHandler}
+                onBlur={blurHandler}
+                onChange={changeHandler}
+              >
+                <div>
+                  <span className={styles["login__form--id--icon"]}></span>
+                </div>
+                <input id="id" placeholder="아이디(이메일)" type="text"></input>
+              </label>
+            </div>
+            <p className={styles["login__form--error"]}>
+              아이디(이메일)를 입력해주세요.
+            </p>
           </div>
 
           <div className={styles.login__main__passwd}>
