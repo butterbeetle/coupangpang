3e3c814f1ef6c53f1d4f01ec2faf3f64996e4efb
butterbeetle <butterbeetle@naver.com>
2022-12-26 00:30:14 +0900

    [105] 오늘의 발견

--- src/Components/Discovery.js
+++ src/Components/Discovery.js
@@ -1,39 +1,28 @@
 import styles from "./Discovery.module.css";
+import DiscoveryItems from "./DiscoveryItems";
+
+const discoveryItems = [
+  { idx: "01", url: "/" },
+  { idx: "02", url: "/" },
+  { idx: "03", url: "/" },
+  { idx: "04", url: "/" },
+  { idx: "05", url: "/" },
+  { idx: "06", url: "/" },
+  { idx: "07", url: "/" },
+  { idx: "08", url: "/" },
+  { idx: "09", url: "/" },
+];
 
 const Discovery = () => {
   return (
-    <article className={styles["contents__discovery"]}>
+    <article className={styles["discovery"]}>
       <div>
-        <span className={styles["contents__discovery--title"]} />
-        <div className={styles["contents__discovery--items"]}>
-          <div className={styles["contents__discovery--items--grid"]}>
-            <div className={[styles.item, styles.today_item1].join(" ")}>
-              item1
-            </div>
-            <div className={[styles.item, styles.today_item2].join(" ")}>
-              item2
-            </div>
-            <div className={[styles.item, styles.today_item3].join(" ")}>
-              item3
-            </div>
-            <div className={[styles.item, styles.today_item4].join(" ")}>
-              item4
-            </div>
-            <div className={[styles.item, styles.today_item5].join(" ")}>
-              item5
-            </div>
-            <div className={[styles.item, styles.today_item6].join(" ")}>
-              item6
-            </div>
-            <div className={[styles.item, styles.today_item7].join(" ")}>
-              item7
-            </div>
-            <div className={[styles.item, styles.today_item8].join(" ")}>
-              item8
-            </div>
-            <div className={[styles.item, styles.today_item9].join(" ")}>
-              item9
-            </div>
+        <span className={styles["discovery__title"]} />
+        <div className={styles["discovery__items"]}>
+          <div className={styles["discovery__grid"]}>
+            {discoveryItems.map((item) => (
+              <DiscoveryItems url={item.url} idx={item.idx} />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/Components/DiscoveryItems.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./DiscoveryItems.module.css";
+
+const DiscoveryItems = ({ url, idx }) => {
+  const [hover, setHover] = useState(false);
+
+  const onMouseEnter = () => {
+    setHover(true);
+  };
+  const onMouseLeave = () => {
+    setHover(false);
+  };
+
+  let buyButton = hover ? (
+    <span className={`${styles["discovery__item--buying"]}`} />
+  ) : (
+    <span className={`${styles["discovery__item--arrow"]}`} />
+  );
+  return (
+    <div
+      className={`${styles["discovery__item"]} ${styles["grid__item" + idx]}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <Link to={url}>
+        <span className={`${styles["discovery__item--img" + idx]}`} />
+        {buyButton}
+      </Link>
+    </div>
+  );
+};
+
+export default DiscoveryItems;
