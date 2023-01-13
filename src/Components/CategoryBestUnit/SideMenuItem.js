6c9d39d66023fa3f3ff91ddc7d8b2687e534f7bd
butterbeetle <butterbeetle@naver.com>
2023-01-13 21:38:24 +0900

    [132] side menu 색상 작업..

--- src/Components/CategoryBestUnit/SideMenuItem.js
+++ src/Components/CategoryBestUnit/SideMenuItem.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import styles from "./SideMenuItem.module.css";
 
 const SideMenuItem = ({ index, title, style }) => {
+  const [hover, setHover] = useState(false);
+
+  const onMouseEnter = () => {
+    setHover(true);
+  };
+  const onMouseLeave = () => {
+    setHover(false);
+  };
+
   return (
-    <li className={styles["list"]} key={index}>
-      <span className={styles[style]} />
-      <span className={styles["title"]}>{title}</span>
+    <li
+      className={styles["list"]}
+      key={index}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <span
+        className={`
+        ${styles["icon"]} 
+        ${hover ? styles[style + "-hover"] : styles[style]} 
+        ${hover ? styles[style + "-color"] : ""}`}
+      />
+      {hover && (
+        <span className={`${styles["title"]} ${styles[style + "-title"]}`}>
+          {title}
+        </span>
+      )}
     </li>
   );
 };
