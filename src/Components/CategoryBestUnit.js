5792184b7903afac8fa715a00f691976f8d011fb
butterbeetle <butterbeetle@naver.com>
2023-01-14 02:26:03 +0900

    [134] scroll 이벤트 테스트

--- src/Components/CategoryBestUnit.js
+++ src/Components/CategoryBestUnit.js
@@ -1,11 +1,13 @@
-import styles from "./CategoryBestUnit.module.css";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import styles from "./CategoryBestUnit.module.css";
 
 import CategoryPromotion from "./CategoryBestUnit/CategoryPromotion";
 import CategoryItemList from "./CategoryBestUnit/CategoryItemList";
 
 import best_title1 from "../img/best_title/best_title_womanclothe2.png";
 import SideMenu from "./CategoryBestUnit/SideMenu";
+
 // const bestUnitItems = [
 //   {
 //     title:"woman-clothes",
@@ -26,9 +28,19 @@ import SideMenu from "./CategoryBestUnit/SideMenu";
 // ]
 
 const CategoryBestUnit = () => {
+  const test = useRef(null);
+  const test2 = useRef(null);
+  const scrollSection = () => {
+    window.scrollTo({
+      top: test2.current.offsetTop + test.current.offsetTop + 600,
+      behavior: "auto",
+    });
+    console.log(test2.current.offsetTop, test.current.offsetTop);
+  };
   return (
-    <article className={styles["best-unit"]}>
-      <SideMenu />
+    <article className={styles["best-unit"]} ref={test2}>
+      <button onClick={scrollSection}>test</button>
+      {/* <SideMenu /> */}
       <span className={styles["title"]} />
       <div className={styles["contents"]}>
         <div className={styles["main"]}>
@@ -53,7 +65,7 @@ const CategoryBestUnit = () => {
           <CategoryItemList />
         </div>
       </div>
-      <div className={styles["contents"]}>
+      <div className={styles["contents"]} ref={test}>
         <div className={styles["main"]}>
           <div className={styles["category"]}>
             <img src={best_title1} alt="Title" />
