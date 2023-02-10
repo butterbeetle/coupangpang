5abac466ce01cf8246e69ec9b05936efa81674cc
butterbeetle <butterbeetle@naver.com>
2023-02-10 22:26:28 +0900

    [login] 대대적 수정.. 회원 약관 모두 체크

--- src/Components/Auth/JoinForm.js
+++ src/Components/Auth/JoinForm.js
@@ -1,82 +1,17 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import useInput from "../../hooks/use-input";
 import { loggedActions } from "../../store";
 import { AuthModal } from "../../UI/AuthModal";
 import styles from "./JoinForm.module.css";
+import Terms from "./Terms";
 
 const EMAIL = 1;
 const PASSWD = 2;
 const PASSWD_CONFIRM = 3;
 const NAME = 4;
 const PHONE = 5;
-
-const essentialTermsData = [
-  {
-    id: "terms_age",
-    text: "[필수] 만 14세 이상입니다",
-    isCheck: false,
-    isArrow: false,
-  },
-  {
-    id: "terms_service",
-    text: "[필수] 쿠팡 이용약관 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-  {
-    id: "terms_commerce",
-    text: "[필수] 전자금융거래 이용약관 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-  {
-    id: "terms_collection_userInfo",
-    text: "[필수] 개인정보 수집 및 이용 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-  {
-    id: "terms_collection_userInfo_thirdParty",
-    text: "[필수] 개인정보 제3자 제공 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-];
-
-const optionalTermsData = [
-  {
-    id: "terms_marketing",
-    text: "[선택] 마케팅 목적의 개인정보 수집 및 이용 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-  {
-    id: "terms_SMS",
-    text: "[선택] 광고성 정보 수신 동의",
-    isCheck: false,
-    isArrow: true,
-  },
-  {
-    id: "terms_SMS_email",
-    text: "[선택] 이메일 수신 동의",
-    isCheck: false,
-    isArrow: false,
-  },
-  {
-    id: "terms_SMS_sms",
-    text: "[선택] SMS, SNS 수신 동의",
-    isCheck: false,
-    isArrow: false,
-  },
-  {
-    id: "terms_SMS_app",
-    text: "[선택] 앱 푸시 수신 동의",
-    isCheck: false,
-    isArrow: false,
-  },
-];
 
 const passwdInit = [
   {
@@ -281,153 +216,12 @@ const JoinForm = () => {
   const stylesInputPhone = phoneInputHasError ? `${styles.invalid}` : "";
   const stylesFocusPhone = phoneFocus ? `${styles.focus}` : "";
 
-  const [isAllCheck, setIsAllCheck] = useState(false);
-
-  const [btnTouch, setBtnTouch] = useState(false);
   const [essentCheck, setEssentCheck] = useState(false);
-
-  const [essentItems, setEssentItems] = useState(essentialTermsData);
-  const [optionItems, setOptionItems] = useState(optionalTermsData);
 
   const [modalInfo, setModalInfo] = useState(null);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const essentArray = [...essentItems];
-    const essentResult = essentArray.filter((item) => item.isCheck === false);
-    if (!essentResult.length) setEssentCheck(true);
-
-    // Check 되지 않은 item 개수
-    const tempArray = [...essentItems, ...optionItems];
-    const result = tempArray.filter((item) => item.isCheck === false);
-
-    // Check 되지 않은 item 개수 가 0이 되면 모두 체크된 것으로 판단함.
-    if (!result.length) {
-      setIsAllCheck(true);
-    } else {
-      setIsAllCheck(false);
-    }
-  }, [essentItems, optionItems]);
-
-  // 모두 확인하였으며 동의합니다.
-  const allCheckHandler = () => {
-    const copyEssentItems = [...essentItems];
-    const copyOptionItems = [...optionItems];
-
-    // Check 되지 않은 [필수] 아이템 개수
-    const filterEssentItems = copyEssentItems.filter(
-      (item) => item.isCheck === false
-    );
-
-    // Check 되지 않은 [선택] 아이템 개수
-    const filterOptionItems = copyOptionItems.filter(
-      (item) => item.isCheck === false
-    );
-
-    if (filterEssentItems.length === 0 && filterOptionItems.length === 0) {
-      // 모두 체크되었다면
-      copyEssentItems.map((item) => (item.isCheck = false));
-      copyOptionItems.map((item) => (item.isCheck = false));
-    } else {
-      // 모두 체크되지 않거나 일부만 체크 되었다면
-      // isCheck가 false인 item을 찾아서 true로 변경함.
-      copyEssentItems
-        .filter((item) => item.isCheck === false)
-        .map((item) => (item.isCheck = true));
-
-      copyOptionItems
-        .filter((item) => item.isCheck === false)
-        .map((item) => (item.isCheck = true));
-    }
-    setEssentItems((prevState) => (prevState = copyEssentItems));
-    setOptionItems((prevState) => (prevState = copyOptionItems));
-  };
-  const stylesTermsIcon = isAllCheck
-    ? styles["terms__icon--on"]
-    : styles["terms__icon--off"];
-
-  // [필수] 아이템 체크박스 눌렀을 때
-  const essentItemCheckHandler = (event) => {
-    // 어느 Item의 체크박스를 체크했는지 htmlfor로 {item.id}를 가져옴.
-    const targetId = event.currentTarget.getAttribute("for");
-
-    let copyItems = [...essentItems];
-    // termsItem의 id와 targetId를 비교해서 찾음
-    const idx = copyItems.findIndex((item) => item.id === targetId);
-
-    // index item의 isCheck를 변경
-    copyItems[idx].isCheck = !copyItems[idx].isCheck;
-
-    setEssentItems((prevState) => (prevState = copyItems));
-  };
-
-  // [선택] 아이템 체크박스 눌렀을 때
-  const optionItemCheckHandler = (event) => {
-    let copyItems = [...optionItems];
-    // 어느 Item의 체크박스를 체크했는지 htmlfor로 {item.id}를 가져옴.
-    const targetId = event.currentTarget.getAttribute("for");
-
-    // termsItem의 id와 targetId를 비교해서 찾음
-    const targetIndex = copyItems.findIndex((item) => item.id === targetId);
-
-    if (targetId === "terms_marketing") {
-      // [선택] 마케팅 목적의 개인정보 수집 및 이용 동의
-      if (copyItems[targetIndex].isCheck) {
-        copyItems
-          .filter((item, idx) => idx >= targetIndex)
-          .map((item) => (item.isCheck = false));
-      } else {
-        copyItems
-          .filter((item, idx) => idx >= targetIndex)
-          .map((item) => (item.isCheck = true));
-      }
-    } else if (targetId === "terms_SMS") {
-      //[선택] 광고성 정보 수신 동의
-      if (copyItems[targetIndex].isCheck) {
-        copyItems
-          .filter((item, idx) => idx >= targetIndex)
-          .map((item) => (item.isCheck = false));
-      } else {
-        if (!copyItems[targetIndex - 1].isCheck) {
-          copyItems[targetIndex - 1].isCheck = true;
-        }
-        copyItems
-          .filter((item, idx) => idx >= targetIndex)
-          .map((item) => (item.isCheck = true));
-      }
-    } else {
-      // [선택] 이메일 수신 동의 / SMS, SNS수신 동의 / 앱 푸시 수신 동의
-      copyItems[targetIndex].isCheck = !copyItems[targetIndex].isCheck;
-      if (copyItems[targetIndex].isCheck) {
-        copyItems
-          .filter(
-            (item) => item.id === "terms_marketing" || item.id === "terms_SMS"
-          )
-          .map((item) => (item.isCheck = true));
-      } else {
-        const smsLength = copyItems
-          .filter(
-            (item) =>
-              item.id === "terms_SMS_email" ||
-              item.id === "terms_SMS_sms" ||
-              item.id === "terms_SMS_app"
-          )
-          .filter((item) => item.isCheck === true);
-        if (smsLength.length === 0) {
-          const idx = copyItems.findIndex((item) => item.id === "terms_SMS");
-          copyItems[idx].isCheck = false;
-        }
-      }
-    }
-
-    setOptionItems((prevState) => (prevState = copyItems));
-  };
-
-  // > 클릭 시 Modal 창
-  const modalClickHandler = (id) => {
-    setModalInfo(id);
-  };
   const modalConfirmHandler = () => {
     setModalInfo(null);
   };
@@ -436,7 +230,7 @@ const JoinForm = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    setBtnTouch(true);
+    // setBtnTouch(true);
 
     if (
       !enteredEmailIsValid &&
@@ -709,87 +503,7 @@ const JoinForm = () => {
               </div>
             </div>
 
-            <section className={styles.terms}>
-              <div className={styles["terms__all"]}>
-                <div
-                  onClick={allCheckHandler}
-                  className={styles["terms__all--icon"]}
-                >
-                  <i className={`${stylesTermsIcon}`}></i>
-                  <p>모두 확인하였으며 동의합니다.</p>
-                </div>
-                <div className={styles["terms__all--text"]}>
-                  <p>
-                    전체 동의에는 필수 및 선택 정보에 대한 동의가 포함되어
-                    있으며, 개별적으로 동의를 선택 하실 수 있습니다. 선택 항목에
-                    대한 동의를 거부하시는 경우에도 서비스 이용이 가능합니다
-                  </p>
-                </div>
-              </div>
-              {btnTouch && !essentCheck && (
-                <div className={styles["terms__error"]}>
-                  <i className={styles["terms__error--icon"]}></i>
-                  <p className={styles["terms__error--text"]}>
-                    필수 항목에 모두 동의해주세요
-                  </p>
-                </div>
-              )}
-              <div className={styles["terms__each"]}>
-                <ul className={styles["terms__each--items"]}>
-                  {essentItems.map((item, idx) => (
-                    <li key={idx} className={styles["terms__each--item"]}>
-                      <div className={styles["terms__each--item--box"]}>
-                        <label
-                          htmlFor={item.id}
-                          onClick={essentItemCheckHandler}
-                        >
-                          {item.isCheck ? (
-                            <i className={styles["terms__icon--on"]}></i>
-                          ) : (
-                            <i className={styles["terms__icon--off"]}></i>
-                          )}
-                          <p>{item.text}</p>
-                        </label>
-                        {item.isArrow ? (
-                          <button
-                            onClick={() => modalClickHandler(item.id)}
-                            className={styles["terms__icon--arrow"]}
-                          ></button>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                    </li>
-                  ))}
-
-                  {optionItems.map((item, idx) => (
-                    <li key={idx} className={styles["terms__each--item"]}>
-                      <div className={styles["terms__each--item--box"]}>
-                        <label
-                          htmlFor={item.id}
-                          onClick={optionItemCheckHandler}
-                        >
-                          {item.isCheck ? (
-                            <i className={styles["terms__icon--on"]}></i>
-                          ) : (
-                            <i className={styles["terms__icon--off"]}></i>
-                          )}
-                          <p>{item.text}</p>
-                        </label>
-                        {item.isArrow ? (
-                          <button
-                            onClick={() => modalClickHandler(item.id)}
-                            className={styles["terms__icon--arrow"]}
-                          ></button>
-                        ) : (
-                          ""
-                        )}
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </section>
+            <Terms />
             <button className={styles["auth-form__button"]}>
               동의하고 가입하기
             </button>
