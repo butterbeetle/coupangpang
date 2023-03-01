ca153f7f94708fec49dfd467e283e7ba38d2c9c1
butterbeetle <butterbeetle@naver.com>
2023-03-01 21:29:37 +0900

    [Products] 상품문의

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -254,6 +254,57 @@ const Product = () => {
               </div>
             </div>
           </li>
+          <li className={styles["contents__enquiry"]}>
+            <div className={styles["enquiry__main"]}>
+              <div className={styles["enquiry__header"]}>
+                <h4>상품문의</h4>
+                <div className={styles["enquiry__button"]}>문의하기</div>
+              </div>
+              <div className={styles["enquiry__caution"]}>
+                <ul>
+                  <li>
+                    구매한 상품의{" "}
+                    <strong>취소/반품은 마이쿠팡 구매내역에서 신청</strong>{" "}
+                    가능합니다.
+                  </li>
+                  <li>
+                    상품문의 및 후기게시판을 통해 취소나 환불, 반품 등은
+                    처리되지 않습니다.
+                  </li>
+                  <li>
+                    <strong>
+                      가격, 판매자, 교환/환불 및 배송 등 해당 상품 자체와 관련
+                      없는 문의는 고객센터 내 1:1 문의하기
+                    </strong>
+                    를 이용해주세요.
+                  </li>
+                  <li>
+                    <strong>
+                      "해당 상품 자체"와 관계없는 글, 양도, 광고성, 욕설, 비방,
+                      도배 등의 글은 예고 없이 이동, 노출제한, 삭제 등의 조치가
+                      취해질 수 있습니다.
+                    </strong>
+                  </li>
+                  <li>
+                    공개 게시판이므로 전화번호, 메일 주소 등 고객님의 소중한
+                    개인정보는 절대 남기지 말아주세요.
+                  </li>
+                </ul>
+              </div>
+              <div className={styles["enquiry__list"]}>
+                <div className={styles["enquiry__nothing"]}>
+                  등록된 문의가 없습니다.
+                </div>
+              </div>
+              <div className={styles["enquiry__report"]}>
+                판매 부적격 상품 또는 허위과장광고 및 지식재산권을 침해하는
+                상품의 경우 신고하여 주시기 바랍니다.
+                <div className={styles["enquiry__report--button"]}>
+                  신고하기
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </section>
