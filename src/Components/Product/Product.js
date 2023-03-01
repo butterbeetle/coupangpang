e5e7e1f50a980f5aabc33762a8efc97a80c0d2aa
butterbeetle <butterbeetle@naver.com>
2023-03-02 00:50:22 +0900

    [Products] 배송/교환/반품 안내

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -305,6 +305,252 @@ const Product = () => {
               </div>
             </div>
           </li>
+          <li className={styles["contents__notice"]}>
+            <div className={styles["notice__delivary__info"]}>
+              <h5>배송정보</h5>
+              <div className={styles["notice__grid"]}>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>배송방법</div>
+                  <div className={styles["notice__content"]}>
+                    신선/냉장/냉동
+                  </div>
+                </div>
+                <div
+                  className={`${styles["notice__item"]} ${styles["row__2"]}`}
+                >
+                  <div className={styles["notice__title"]}>배송비</div>
+                  <div className={styles["notice__content"]}>
+                    무료배송
+                    <br />
+                    - 도서산간 추가 배송비
+                    <br />
+                    - 제주 지역: 5,000원
+                    <br />
+                    - 도서산간 지역: 5,000원
+                    <br />
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>배송사</div>
+                  <div className={styles["notice__content"]}>로젠택배</div>
+                </div>
+                <div
+                  className={`${styles["notice__item"]} ${styles["column__2"]}`}
+                >
+                  <div className={styles["notice__title"]}>묶음배송 여부</div>
+                  <div className={styles["notice__content"]}>가능</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles["notice__delivary__return"]}>
+              <h5>교환/반품 안내</h5>
+              <ul>
+                <li>
+                  ㆍ교환/반품에 관한 일반적인 사항은 판매자가 제시사항보다
+                  관계법령이 우선합니다.
+                  <br />
+                  다만, 판매자의 제시사항이 관계법령보다 소비자에게 유리한
+                  경우에는 판매자 제시사항이 적용됩니다.
+                </li>
+              </ul>
+              <div className={styles["notice__grid"]}>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    교환/반품 비용
+                    <br />
+                    (왕복비용)
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    3,500원
+                    <br />
+                    - 단, 고객 변심의 경우에만 발생
+                    <br />- 도서산간 및 일부 지역 추가비용 발생
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    교환/반품 신청 기준일
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <p>
+                      ㆍ단순변심에 의한 교환/반품은 제품 수령 후 7일 이내까지,
+                      교환/반품 제한사항에 해당하지 않는 경우에만 가능
+                      <p className={styles["indent"]}>
+                        (배송비용과 교환/반품 비용 왕복배송비 고객부담)
+                      </p>
+                      <br />
+                      ㆍ상품의 내용이 표시·광고의 내용과 다른 경우에는 상품을
+                      수령한 날부터 3개월 이내, 그 사실을 안 날 또는 알 수
+                      있었던 날부터
+                    </p>
+                    <p className={styles["indent"]}>
+                      30일 이내에 청약철회 가능
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles["notice__delivary__limit"]}>
+              <h5>교환/반품 제한사항</h5>
+              <ul>
+                <li>ㆍ주문/제작 상품의 경우, 상품의 제작이 이미 진행된 경우</li>
+                <li>
+                  ㆍ상품 포장을 개봉하여 사용 또는 설치 완료되어 상품의 가치가
+                  훼손된 경우 (단, 내용 확인을 위한 포장 개봉의 경우는 예외)
+                </li>
+                <li>
+                  ㆍ고객의 사용, 시간경과, 일부 소비에 의하여 상품의 가치가
+                  현저히 감소한 경우
+                </li>
+                <li>
+                  ㆍ세트상품 일부 사용, 구성품을 분실하였거나 취급 부주의로 인한
+                  파손/고장/오염으로 재판매 불가한 경우
+                </li>
+                <li>
+                  ㆍ모니터 해상도의 차이로 인해 색상이나 이미지가 실제와 달라,
+                  고객이 단순 변심으로 교환/반품을 무료로 요청하는 경우
+                </li>
+                <li>
+                  ㆍ제조사의 사정 (신모델 출시 등) 및 부품 가격 변동 등에 의해
+                  무료 교환/반품으로 요청하는 경우
+                </li>
+              </ul>
+              <p className={styles["limit__text"]}>
+                ※ 각 상품별로 아래와 같은 사유로 취소/반품이 제한될 수 있습니다.
+              </p>
+              <div className={styles["notice__grid"]}>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    의류/잡화/수입명품
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    ㆍ상품의 택(TAG) 제거, 라벨 및 상품 훼손, 구성품 누락으로
+                    상품의 가치가 현저히 감소된 경우
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    계절상품/식품/화장품
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <p>
+                      ㆍ신선/냉장/냉동 식품의 단순변심의 경우
+                      <br />
+                      ㆍ뷰티 상품 이용 시 트러블(알러지, 붉은 반점, 가려움,
+                      따가움)이 발생하는 경우,
+                    </p>
+                    <p className={styles["indent"]}>
+                      진료 확인서 및 소견서 등을 증빙하면 환불이 가능 (제반비용
+                      고객부담)
+                    </p>
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    전자/가전/설치상품
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <p>
+                      ㆍ설치 또는 사용하여 재판매가 어려운 경우, 액정이 있는
+                      상품의 전원을 켠 경우
+                      <br />
+                      ㆍ상품의 시리얼 넘버 유출로 내장된 소프트웨어의 가치가
+                      감소한 경우 (내비게이션, OS시리얼이 적힌 PMP)
+                      <br />
+                      ㆍ홀로그램 등을 분리, 분실, 훼손하여 상품의 가치가 현저히
+                      감소하여 재판매가 불가할 경우 (노트북, 데스크탑 PC 등)
+                    </p>
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>자동차용품</div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <p>ㆍ상품을 개봉하여 장착한 이후 단순변심인 경우</p>
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    CD/DVD/GAME/ BOOK
+                  </div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <p>ㆍ복제가 가능한 상품의 포장 등을 훼손한 경우</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles["notice__delivary__seller"]}>
+              <h5>판매자 정보</h5>
+              <div className={`${styles["notice__grid"]} ${styles["grid__2"]}`}>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>상호/대표자</div>
+                  <div className={styles["notice__content"]}>
+                    주식회사 두손식품 / 윤진
+                  </div>
+                </div>
+                <div className={`${styles["notice__item"]}`}>
+                  <div className={styles["notice__title"]}>사업장 소재지</div>
+                  <div className={styles["notice__content"]}>
+                    인천광역시 남동구 운연로 135-1 (운연동) 1층
+                  </div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>e-mail</div>
+                  <div className={styles["notice__content"]}>
+                    dusonfood@naver.com
+                  </div>
+                </div>
+                <div className={`${styles["notice__item"]}`}>
+                  <div className={styles["notice__title"]}>연락처</div>
+                  <div className={styles["notice__content"]}>02-1644-9914</div>
+                </div>
+                <div className={styles["notice__item"]}>
+                  <div className={styles["notice__title"]}>
+                    통신판매업
+                    <br />
+                    신고번호
+                  </div>
+                  <div className={styles["notice__content"]}>
+                    2019-인천남동구-1394
+                  </div>
+                </div>
+                <div className={`${styles["notice__item"]}`}>
+                  <div className={styles["notice__title"]}>사업자번호</div>
+                  <div className={styles["notice__content"]}>603-87-01605</div>
+                </div>
+                <div
+                  className={`${styles["notice__item"]} ${styles["column__2"]}`}
+                >
+                  <div className={styles["notice__title"]}>구매안전 서비스</div>
+                  <div
+                    className={`${styles["notice__content"]} ${styles["notice__width"]}`}
+                  >
+                    <div className={styles["notice__service__join"]}>
+                      02-123-4567
+                      <p>{"서비스 가입사실 확인 >"}</p>
+                    </div>
+                    <p className={styles["escrow"]}>
+                      본 판매자는 고객님의 안전거래를 위해 관련 법률에 의거하여
+                      쿠팡페이의 구매안전서비스를 적용하고 있습니다.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
     </section>
