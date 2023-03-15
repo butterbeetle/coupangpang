d15778ba8c8d2aaab42427b14cb36cfa8255f776
butterbeetle <butterbeetle@naver.com>
2023-03-16 03:27:58 +0900

    [Cart] 상품 선택에 따라 총 가격 변경

--- src/Components/Cart/CartMain/Content/CartCalc.js
+++ src/Components/Cart/CartMain/Content/CartCalc.js
@@ -1,16 +1,35 @@
 import styles from "./CartCalc.module.css";
 import { MdAddCircle, MdPauseCircle } from "react-icons/md";
+import { useSelector } from "react-redux";
 const CartCalc = () => {
+  /* 장바구니에 있는 Item 정보 */
+  const cartItems = useSelector((state) => state.cart.items);
+  /* 장바구니 check 정보 */
+  const checked = useSelector((state) => state.cart.checked);
+
+  const totalPrice = cartItems.reduce((acc, { id, totalPrice }) => {
+    if (checked.includes(id)) {
+      acc += totalPrice;
+    }
+    return acc;
+  }, 0);
+
   return (
     <div className={styles["calc"]}>
       <span>상품가격</span>
-      <span className={styles["calc__price"]}>58,700</span>원
+      <span className={styles["calc__price"]}>
+        {totalPrice.toLocaleString()}
+      </span>
+      원
       <MdAddCircle className={styles["plus"]} />
       <span>배송비</span>
       <span className={styles["calc__delivary"]}>0</span>원
       <MdPauseCircle className={styles["equal"]} />
       <span>주문금액</span>
-      <span className={styles["calc__total"]}>58,710</span>원
+      <span className={styles["calc__total"]}>
+        {totalPrice.toLocaleString()}
+      </span>
+      원
     </div>
   );
 };
--- src/Components/Cart/CartMain/Content/CartTotal.js
+++ src/Components/Cart/CartMain/Content/CartTotal.js
@@ -2,19 +2,34 @@ import styles from "./CartTotal.module.css";
 /* Icon */
 import { AiOutlinePlusSquare } from "react-icons/ai";
 import { MdPausePresentation } from "react-icons/md";
+import { useSelector } from "react-redux";
 const CartTotal = () => {
+  /* 장바구니에 있는 Item 정보 */
+  const cartItems = useSelector((state) => state.cart.items);
+  /* 장바구니 check 정보 */
+  const checked = useSelector((state) => state.cart.checked);
+
+  const totalPrice = cartItems.reduce((acc, { id, totalPrice }) => {
+    if (checked.includes(id)) {
+      acc += totalPrice;
+    }
+    return acc;
+  }, 0);
   return (
     <div className={styles["total"]}>
       <div className={styles["total__container"]}>
         총 상품가격
-        <span className={styles["total__price"]}>0</span>원
+        <span className={styles["total__price"]}>
+          {totalPrice.toLocaleString()}
+        </span>
+        원
         <AiOutlinePlusSquare className={styles["plus-square"]} />총 배송비
         <span className={styles["total__price"]}>0</span>원
         <MdPausePresentation className={styles["equal-square"]} />총 주문금액
         <span
           className={`${styles["total__price"]} ${styles["total__price__final"]}`}
         >
-          0원
+          {totalPrice.toLocaleString()}원
         </span>
       </div>
     </div>
