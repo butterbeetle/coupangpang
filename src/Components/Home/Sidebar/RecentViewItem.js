b2500adc9795b8a40a0af7b42d0a06552a510dd2
butterbeetle <butterbeetle@naver.com>
2023-03-21 03:20:11 +0900

    [RecentView] 최근 본 상품, 마우스 오버 이벤트 수정, 클릭 시 해당 상품으로

--- src/Components/Home/Sidebar/RecentViewItem.js
+++ src/Components/Home/Sidebar/RecentViewItem.js
@@ -3,6 +3,7 @@ import styles from "./RecentViewItem.module.css";
 
 /* Icon */
 import { BsFillXSquareFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const RecentViewItem = ({ item }) => {
   const [isHover, setIsHover] = useState(false);
@@ -14,25 +15,33 @@ const RecentViewItem = ({ item }) => {
   };
 
   return (
-    <div className={styles["item"]}>
-      <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+    <div
+      className={styles["item"]}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <Link to={`/products/${item.id}`}>
         <img src={item.realUrl} alt="" />
-      </div>
 
-      {isHover && (
-        <div className={styles["item__infos"]}>
-          <BsFillXSquareFill className={styles["cancel"]} title="닫기" />
-          <div className={styles["info"]}>
-            <span className={styles["title"]}>{item.title}</span>
-            <span className={styles["price"]}>
-              <p className={styles["price__value"]}>
-                {item.price.toLocaleString()}
-              </p>
-              원
-            </span>
+        {
+          <div
+            className={`${styles["item__infos"]} ${
+              isHover ? styles["visible"] : styles["none"]
+            }`}
+          >
+            <BsFillXSquareFill className={styles["cancel"]} title="닫기" />
+            <div className={styles["info"]}>
+              <span className={styles["title"]}>{item.title}</span>
+              <span className={styles["price"]}>
+                <p className={styles["price__value"]}>
+                  {item.price.toLocaleString()}
+                </p>
+                원
+              </span>
+            </div>
           </div>
-        </div>
-      )}
+        }
+      </Link>
     </div>
   );
 };
