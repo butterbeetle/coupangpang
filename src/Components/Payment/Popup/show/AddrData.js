0ea937a716b948d329249c08575cbc8c5a0bcb7e
butterbeetle <butterbeetle@naver.com>
2023-04-28 20:46:39 +0900

    Style : 기본배송지 표시 추가
    이름 밑, 주소 위에 기본 배송지 표시 추가함.

--- src/Components/Payment/Popup/show/AddrData.js
+++ src/Components/Payment/Popup/show/AddrData.js
@@ -4,12 +4,16 @@ const AddrData = ({ item }) => {
   const phone = item.phone.replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3");
   const normal = item.delivaryNormal ? item.delivaryNormal : "문 앞";
   const dawn = item.delivaryDawn ? item.delivaryDawn : "";
+
   return (
     <div className={`${styles["card"]} ${styles["card-select"]}`}>
       <div className={styles["main__header"]}>
         <p className={styles["name"]}>{item.name}</p>
       </div>
       <div className={styles["main__body"]}>
+        {item.default_setting && (
+          <p className={styles["default"]}>기본배송지</p>
+        )}
         <p
           className={styles["addr"]}
         >{`${item.roadAddress}, ${item.detailAddress}`}</p>
