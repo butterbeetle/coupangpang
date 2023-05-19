f70087561c0cd1b6e208add87b45a56716938cc1
butterbeetle <butterbeetle@naver.com>
2023-05-19 22:29:14 +0900

    Fix : 장바구니에서 선택한 아이템 표시
    1. 장바구니에서 선택한 아이템들이 제대로 표시되도록 수정함.
    2. PaymenItem 컴포넌트 새로 제작함.

--- src/Components/Payment/Main/PaymentDelivary.js
+++ src/Components/Payment/Main/PaymentDelivary.js
@@ -1,20 +1,20 @@
 import { useSelector } from "react-redux";
 import styles from "./PaymentDelivary.module.css";
 import { useState } from "react";
+import PaymentItem from "./PaymentItem";
 
 const PaymentDelivary = () => {
-  const cartItem = useSelector((state) => state.cart);
+  const checked = useSelector((state) => state.cart.checked);
+  const cartItem = useSelector((state) => state.cart.items);
   const [purchasedItem] = useState(
-    cartItem.items.filter((item) => {
-      for (let id of cartItem.checked) {
-        if (id === item.id) return item;
-      }
-      return [];
-    })
+    cartItem.filter((item) => checked.includes(item.id))
   );
+
   const day = ["일", "월", "화", "수", "목", "금", "토"];
   const today = new Date();
   const tomorrow = new Date(today.setDate(today.getDate() + 1));
+
+  console.log(cartItem, purchasedItem);
   return (
     <div className={styles["content"]}>
       <div className={styles["title"]}>
@@ -23,23 +23,11 @@ const PaymentDelivary = () => {
       <div className={styles["customer"]}>
         <div className={styles["customer__info"]}>
           <div className={styles["customer__info__header"]}>{`내일(${
-            day[tomorrow.getDay() + 1]
+            day[tomorrow.getDay()]
           }) ${tomorrow.getMonth() + 1}/${tomorrow.getDate()} 도착 보장`}</div>
-          <div className={styles["customer__info__content"]}>
-            <div className={styles["prod__title"]}>
-              360도 회전가능한 마그네틱 자석 휴대폰 고속 충전 케이블, C타입,
-              레드
-            </div>
-            <div className={styles["prod__quantity"]}>수량 1개 / 무료배송</div>
-          </div>
-
-          <div className={styles["customer__info__content"]}>
-            <div className={styles["prod__title"]}>
-              360도 회전가능한 마그네틱 자석 휴대폰 고속 충전 케이블, C타입,
-              레드
-            </div>
-            <div className={styles["prod__quantity"]}>수량 1개 / 무료배송</div>
-          </div>
+          {purchasedItem.map((item) => (
+            <PaymentItem key={item.id} item={item} />
+          ))}
         </div>
       </div>
     </div>
