b0cb12c4bf410eb410fb69581d5926ee13694625
butterbeetle <butterbeetle@naver.com>
2023-06-08 16:34:04 +0900

    Chore : nextjs 도입 실패 후 처리

--- src/Components/CategoryBestUnit/CategoryPromotion.js
+++ src/Components/CategoryBestUnit/CategoryPromotion.js
@@ -1126,7 +1126,7 @@ const items = {
 const CategoryPromotion = ({ category }) => {
   const [btnHover, setBtnHover] = useState(false);
   const { count, start, stop, reset, resetNum } = useInterval(0, 2000);
-  const len = items[`${category}`].length - 1;
+  const len = items[`${category}`]?.length - 1;
 
   useEffect(() => {
     if (count > len) {
--- src/Components/CategoryBestUnit/Contents.js
+++ src/Components/CategoryBestUnit/Contents.js
@@ -8,9 +8,9 @@ import { Link } from "react-router-dom";
 const Contents = ({ items }) => {
   return (
     <div className={styles["contents"]}>
-      <div className={`${styles["main"]} ${icons[items.styles + "-border"]}`}>
+      <div className={`${styles["main"]} ${icons[items?.styles + "-border"]}`}>
         <div className={styles["category"]}>
-          <span className={`${styles["title"]} ${styles[items.styles]}`} />
+          <span className={`${styles["title"]} ${styles[items?.styles]}`} />
           <Link to="/">
             <span className={styles["shortcut"]}>{"바로가기 >"}</span>
           </Link>
@@ -18,16 +18,16 @@ const Contents = ({ items }) => {
         <div className={styles["keyword"]}>
           <h4>HOT키워드</h4>
           <ul>
-            {items.keywords.map((keyword, idx) => (
+            {items?.keywords.map((keyword, idx) => (
               <li
-                className={icons[items.styles + "-color"]}
+                className={icons[items?.styles + "-color"]}
                 key={idx}
               >{`#${keyword}`}</li>
             ))}
           </ul>
         </div>
-        <CategoryPromotion category={items.styles} />
-        <CategoryItemList category={items.styles} />
+        <CategoryPromotion category={items?.styles} />
+        <CategoryItemList category={items?.styles} />
       </div>
     </div>
   );
