22f3faa96aa7f922e9e997278932df89f8bd762e
butterbeetle <butterbeetle@naver.com>
2023-06-12 21:38:26 +0900

    CHORE : 사용하지 않는 변수 삭제
    사용하지 않는 Link 삭제함.

--- src/Components/Header/MyCoupang/index.js
+++ src/Components/Header/MyCoupang/index.js
@@ -1,7 +1,6 @@
 import styles from "./index.module.css";
 /* Hook */
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 const HeaderMyCoupang = () => {
   const [mouseEnter, setMouseEnter] = useState(false);
