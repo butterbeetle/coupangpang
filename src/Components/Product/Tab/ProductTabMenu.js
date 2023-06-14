811ec2d3b49cc49ef71241243e6bb1d110a29921
butterbeetle <butterbeetle@naver.com>
2023-06-14 23:19:44 +0900

    STYLES : 상품페이지 수정
    각 탭을 클릭했을 때 css 바뀌도록 수정함.

--- src/Components/Product/Tab/ProductTabMenu.js
+++ src/Components/Product/Tab/ProductTabMenu.js
@@ -5,6 +5,7 @@ const ProductTabMenu = ({ refArray }) => {
   const ulRef = useRef();
   const [scroll, setScroll] = useState(0);
   const [scrollAct, setScrollAct] = useState(false);
+  const [menuIdx, setMenuIdx] = useState(0);
 
   const scrollFixed = useCallback(() => {
     if (scroll > 900) {
@@ -28,6 +29,7 @@ const ProductTabMenu = ({ refArray }) => {
   }, [scrollFixed]);
 
   const onClick = (idx) => {
+    setMenuIdx(idx);
     refArray.current[idx].scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -38,10 +40,30 @@ const ProductTabMenu = ({ refArray }) => {
       className={`${styles["tab-menu"]} ${scrollAct ? styles["fixed"] : ""}`}
       ref={ulRef}
     >
-      <li onClick={() => onClick(0)}>상품상세</li>
-      <li onClick={() => onClick(1)}>상품평</li>
-      <li onClick={() => onClick(2)}>상품문의</li>
-      <li onClick={() => onClick(3)}>배송/교환/반품 안내</li>
+      <li
+        onClick={() => onClick(0)}
+        className={`${styles["menu"]} ${menuIdx === 0 ? styles["active"] : ""}`}
+      >
+        상품상세
+      </li>
+      <li
+        onClick={() => onClick(1)}
+        className={`${styles["menu"]} ${menuIdx === 1 ? styles["active"] : ""}`}
+      >
+        상품평
+      </li>
+      <li
+        onClick={() => onClick(2)}
+        className={`${styles["menu"]} ${menuIdx === 2 ? styles["active"] : ""}`}
+      >
+        상품문의
+      </li>
+      <li
+        onClick={() => onClick(3)}
+        className={`${styles["menu"]} ${menuIdx === 3 ? styles["active"] : ""}`}
+      >
+        배송/교환/반품 안내
+      </li>
     </ul>
   );
 };
