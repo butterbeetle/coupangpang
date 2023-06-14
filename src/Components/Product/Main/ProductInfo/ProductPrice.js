01be120ac673494b4addfccb133cb4058fd4f861
butterbeetle <butterbeetle@naver.com>
2023-06-14 21:18:38 +0900

    FIX : 상품 페이지 가격 수정
    할인률이 1% 이상일 때만 보이도록 변경

--- src/Components/Product/Main/ProductInfo/ProductPrice.js
+++ src/Components/Product/Main/ProductInfo/ProductPrice.js
@@ -5,33 +5,38 @@ import { AiOutlineInfo } from "@react-icons/all-files/ai/AiOutlineInfo";
 import { ImCoinDollar } from "@react-icons/all-files/im/ImCoinDollar";
 
 const ProductPrice = () => {
-  const prodDiscount = useSelector((state) => state.prod.discount) ?? 0;
+  const discount = useSelector((state) => state.prod.discount) ?? 0;
   const { price } = useSelector((state) => state.prod.current);
 
   return (
     <div className={styles["product__info--price"]}>
-      <div className={styles["price__discount"]}>
-        <p className={styles["price__discount__rate"]}>{prodDiscount}%</p>
-        <p className={styles["price__origin"]}>{price.toLocaleString()}원</p>
-        <AiOutlineInfo />
-      </div>
-      <div className={styles["price__sale"]}>
+      {discount > 0 && (
+        <div className={styles["price__discount"]}>
+          <p className={styles["price__discount__rate"]}>{discount}%</p>
+          <p className={styles["price__origin"]}>{price.toLocaleString()}원</p>
+          <AiOutlineInfo />
+        </div>
+      )}
+      {/* <div className={styles["price__sale"]}>
         <p className={styles["price__total"]}>{price.toLocaleString()}원</p>
         <p className={styles["price__info"]}>쿠팡판매가</p>
-      </div>
-      {/* <div className={styles["price__coupon"]}>
-        <p className={styles["price__total"]}>
-          {(price - price * prodDiscount * 0.01).toLocaleString()}원
-        </p>
-        <p className={styles["price__info"]}>와우할인가</p>
       </div> */}
+      <div className={styles["price__coupon"]}>
+        <p className={styles["price__total"]}>
+          {(
+            Math.round((price - price * discount * 0.01) / 10) * 10
+          ).toLocaleString()}
+          원
+        </p>
+        {/* <p className={styles["price__info"]}>와우할인가</p> */}
+      </div>
       <div>
         <div className={styles["price__badge"]}>
           <ImCoinDollar />
           <p>
             최대{" "}
             {parseInt(
-              (price - price * prodDiscount * 0.01) * 0.01
+              (price - price * discount * 0.01) * 0.01
             ).toLocaleString()}
             원 적립
           </p>
