5ffaeb35d8b023b92d5ea1f4b5281a769981f43e
butterbeetle <butterbeetle@naver.com>
2023-06-16 21:57:19 +0900

    FIX : 카테고리 별 최고상품 Lazy 로딩
    CategoryBestUnit에 Lazy Loading

--- src/Components/CategoryBestUnit/CategoryBestUnit.js
+++ src/Components/CategoryBestUnit/CategoryBestUnit.js
@@ -1,20 +1,20 @@
 import styles from "./CategoryBestUnit.module.css";
-
+import { lazy, useContext } from "react";
 import SideMenu from "./SideMenu/SideMenu";
-import Contents from "./Contents";
+/* Context */
 import { SideMenuContext } from "../../store/sideMenu-context";
-import { useContext } from "react";
+
+const Contents = lazy(() => import("./Contents"));
 
 const CategoryBestUnit = () => {
   const sideCtx = useContext(SideMenuContext);
 
   return (
     <article className={styles["best-unit"]}>
-      <span className={styles["title"]} />
-      {sideCtx.scrollY > 2000 &&
-        sideCtx.sideBarItems.map(
-          (items) => items.visible && <Contents key={items.key} items={items} />
-        )}
+      <span className={styles["title"]} />=
+      {sideCtx.sideBarItems.map(
+        (items) => items.visible && <Contents key={items.key} items={items} />
+      )}
       {sideCtx.scrollY > 3300 && <SideMenu />}
     </article>
   );
